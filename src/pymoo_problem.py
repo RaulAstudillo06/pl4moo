b72d2df6d29be26d0241c3bbfefab97bde4703c9
RaulAstudillo06 <astudillo0606@gmail.com>
2023-06-27 23:58:01 -0700

    PyMOO problem module

---
+++ src/pymoo_problem.py
@@ -0,0 +1,20 @@
+from pymoo.core.problem import Problem
+import torch
+import numpy as np
+
+
+class PymooProblem(Problem):
+
+    def __init__(self, var, bounds, acq_function):
+        super().__init__(n_var=var, n_obj=1, n_constr=0, xl=bounds[0], xu=bounds[1])
+        self.acquisition_function = acq_function
+
+    def _evaluate(self, x, out, *args, **kwargs):
+        # x has to be converted to Tensor
+
+        x = x.reshape((x.shape[0], 1, x.shape[1]))
+        x = torch.from_numpy(x)
+        acq_evaluation = self.acquisition_function(x)
+        acq_evaluation = acq_evaluation.detach()
+        #acq_evaluation will be a tensor. It has to be converted to numpy
+        out["F"] = np.asarray([acq_evaluation.numpy()]).T
