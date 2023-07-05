61d29f77094ce6d600a0765371fb75ec6274397c
RaulAstudillo06 <astudillo0606@gmail.com>
2023-07-04 23:01:24 -0700

    Negate acquisition function value

--- src/pymoo_problem.py
+++ src/pymoo_problem.py
@@ -4,7 +4,6 @@ import numpy as np
 
 
 class PymooProblem(Problem):
-
     def __init__(self, var, bounds, acq_function):
         super().__init__(n_var=var, n_obj=1, n_constr=0, xl=bounds[0], xu=bounds[1])
         self.acquisition_function = acq_function
@@ -14,7 +13,6 @@ class PymooProblem(Problem):
 
         x = x.reshape((x.shape[0], 1, x.shape[1]))
         x = torch.from_numpy(x)
-        acq_evaluation = self.acquisition_function(x)
-        acq_evaluation = acq_evaluation.detach()
-        #acq_evaluation will be a tensor. It has to be converted to numpy
+        acq_evaluation = -self.acquisition_function(x).detach()
+        # acq_evaluation will be a tensor. It has to be converted to numpy
         out["F"] = np.asarray([acq_evaluation.numpy()]).T
