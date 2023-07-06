4252b5704d0d93eff09fdabe4cc0af3eb6972c98
RaulAstudillo06 <astudillo0606@gmail.com>
2023-07-05 21:49:25 -0700

    Black formatter

--- src/models/composite_model.py
+++ src/models/composite_model.py
@@ -4,16 +4,22 @@ from torch import Tensor
 
 from src.models.pairwise_kernel_variational_gp import PairwiseKernelVariationalGP
 
+
 class CompositePreferentialGP(Model):
     def __init__(
-        self, queries: Tensor, responses: Tensor, attribute_func: Tensor,
+        self,
+        queries: Tensor,
+        responses: Tensor,
+        attribute_func: Tensor,
     ) -> None:
         super().__init__()
         self.queries = queries
         self.responses = responses
         self.attribute_func = attribute_func
-        self.utility_model = PairwiseKernelVariationalGP(attribute_func(queries), responses)
-        
+        self.utility_model = PairwiseKernelVariationalGP(
+            attribute_func(queries), responses
+        )
+
     def posterior(self, X: Tensor, posterior_transform=None) -> MultivariateNormal:
         Y = self.attribute_func(X)
         return self.utility_model.posterior(Y)
